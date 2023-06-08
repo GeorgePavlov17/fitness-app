0fac72e9ace572137accee5127791d27b89cb199
Georgi Pavlov <georgipavlov17@gmail.com>
2023-06-08 17:17:45 +0300

    add datepicker

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,18 +1,42 @@
-import { NgModule } from "@angular/core";
+import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { MatFormFieldModule } from '@angular/material/form-field'
-import { MatInputModule } from '@angular/material/input'
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule, NativeDateAdapter, DateAdapter, MAT_DATE_FORMATS } from '@angular/material/core';
 
 @NgModule({
-    imports: [MatButtonModule,
-              MatIconModule,
-              MatFormFieldModule,
-              MatInputModule],
-    exports: [MatButtonModule,
-              MatIconModule,
-              MatFormFieldModule,
-              MatInputModule]
+  imports: [
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule
+  ],
+  exports: [
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule
+  ],
+  providers: [
+    { provide: DateAdapter, useClass: NativeDateAdapter },
+    { provide: MAT_DATE_FORMATS, useValue: {
+        parse: {
+          dateInput: 'input',
+        },
+        display: {
+          dateInput: 'input',
+          monthYearLabel: 'MMM YYYY',
+          dateA11yLabel: 'LL',
+          monthYearA11yLabel: 'MMMM YYYY',
+        },
+      },
+    },
+  ],
 })
-
-export class MaterialModule {}
+export class MaterialModule { }
