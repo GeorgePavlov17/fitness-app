6af53b035ea332650b4284a5643b2dcd24146cb6
Georgi Pavlov <georgipavlov17@gmail.com>
2023-06-09 22:33:57 +0300

    adding login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { FlexLayoutModule } from '@angular/flex-layout';
 
@@ -45,7 +45,8 @@ import { StopTrainingComponent } from './training/current-training/stop-training
     AppRoutingModule,
     FormsModule,
     MaterialModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
