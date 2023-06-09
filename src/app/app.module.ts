3ae3db203dbef9ad933f9af65af2a2701b41df86
Georgi Pavlov <georgipavlov17@gmail.com>
2023-06-10 00:23:44 +0300

    adding auth service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,6 +21,7 @@ import { WelcomeComponent } from './welcome/welcome.component';
 import { HeaderComponent } from './navigation/header/header.component';
 import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.component';
 import { StopTrainingComponent } from './training/current-training/stop-training.component';
+import { AuthService } from './auth/auth.service';
 
 @NgModule({
   declarations: [
@@ -48,7 +49,7 @@ import { StopTrainingComponent } from './training/current-training/stop-training
     FlexLayoutModule,
     ReactiveFormsModule
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
