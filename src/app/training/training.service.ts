fa3ea7294a40e95037bb2c3a182ef643966b9ff7
Georgi Pavlov <georgipavlov17@gmail.com>
2023-06-10 13:02:19 +0300

    using the Service to start training + adding a Form to training component

--- src/app/training/training.service.ts
+++ src/app/training/training.service.ts
@@ -1,6 +1,10 @@
+import { Subject } from "rxjs";
+
 import { Exercise } from "./exercise.model";
 
 export class TrainingService {
+    exerciseChanged = new Subject<Exercise>();
+
     private availableExercises: Exercise[] = [
         { id: 'biceps', name: 'Biceps', duration: 20, calories: 20 },
         { id: 'triceps', name: 'Triceps', duration: 20, calories: 20 },
@@ -8,7 +12,14 @@ export class TrainingService {
         { id: 'legs', name: 'Legs', duration: 30, calories: 50 },
     ];
 
+    private runningExercise: Exercise | any;
+
     getAvailableExercises() {
         return this.availableExercises.slice();
+    }
+
+    startExercise(selectedId: string) {
+        this.runningExercise = this.availableExercises.find(ex => ex.id === selectedId);
+        this.exerciseChanged.next({ ...this.runningExercise });
     }
 }
